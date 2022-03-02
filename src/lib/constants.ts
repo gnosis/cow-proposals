6342c1f0035f4f06bbef9bc8e491c03242b6bf83
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2022-03-02 18:18:21 +0100

    Iinitial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const realityModule = {
+  // Note that this was added in the snapshot transaction that enabled the Reality module for the Gnosis DAO.
+  // https://snapshot.org/#/gnosis.eth/proposal/QmNMRBnipvRfos9ze1MrKpFAsHqxhNrmxjrXzxJnhFif9b
+  "1": "0x0ebac21f7f6a6599b5fa5f57baaa974adfec4613",
+} as const;
